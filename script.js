26325c4e86c7738d540863ac625697bff482190e
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-03-01 14:43:11 +0000

    Solutions added

--- script.js
+++ script.js
@@ -4,8 +4,57 @@ function setup() {
 
 }
 
+var r = 0
+var g = 0
+
 function draw() {
-	background(200);
+	// Challenge #1:
+	// r = map(mouseX, 0, width, 0, 255)
+	// g = map(mouseY, 0, height, 0, 255)
+	// background(r, g, 0 );
+	
+	// Challenge #2:
+	background(200)
+	// fill('black')
+	// rect(width/2, 0, width/2, height)
+	// noStroke()
+	// if(mouseX > width/2){
+	// 	fill(200)
+	// }
+	// else{
+	// 	fill(0)
+	// }
+	// ellipse(mouseX, mouseY, 20, 20)
+
+	// Challenge #3:
+	// ellipse(width/2, height/2, 50, 50)
+	// var d = dist(mouseX, mouseY, width/2, height/2)
+	// if(d < 25){
+	// 	fill(0)
+	// }
+	// else{
+	// 	fill(255)
+	// }
+
+	// Challenge #4:
+	// var d = dist(mouseX, mouseY, 300, 300)
+	// var d2 = dist(mouseX, mouseY, 150, 300)
+	
+	// if(d < 25){
+	// 	fill("blue")
+	// }
+	// else{
+	// 	fill(255)
+	// }
+	// ellipse(300, 300, 50, 50)
+	// if(d2 < 25){
+	// 	fill("green")
+	// }
+	// else{
+	// 	fill(255)
+	// }
+	// ellipse(150, 300, 50, 50)
+	
 	
 
 }
